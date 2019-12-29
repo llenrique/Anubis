c473373281d4a45711b0b4726e4504c519ce8d91
EL <llenriquelopez@gmail.com>
2019-12-29 14:10:11 -0600

    Refactor

--- lib/anubis/pet.ex
+++ lib/anubis/pet.ex
@@ -1,191 +1,94 @@
 defmodule Anubis.Pet do
-  @moduledoc """
-  Abubis is the library for managing pets information. The intention of this
-  module is to create a pet with the information given y the owner like name,
-  adoption_age, adoption_date and if it's possible, the birth_date.
-
-  This module actually can create a basic structure for a pet, calculate the age in weeks for the pet since the bith_date, determinate if the pet si an
-  adult or a puppy, and in the worst case you can set your pet as death passing 
-  the death_date to the update_death_date_for_pet.
-
-  This module can actually determinate if a puppy can be set in adoption if the
-  weeks sicne birth_date is greater than 12. Otherwise the puppy cannot be set
-  in adoption. For adult pets not age validation is required.
-  """
-
+  
+  defstruct name: nil,
+            species: nil,
+            race: nil,
+            gender: nil,
+            color: nil,
+            birth_date: nil,
+            race_size: nil,
+            particular_signs: nil,
+            age_on_weeks: 0,
+            age_status: nil,
+            adoption_status: false,
+            adoption_date: Date.utc_today()
+  
+  @adult_for_small_dogs 78
+  @adult_for_big_dogs 104
+  @weeks_for_adoption 12
+  
   alias __MODULE__
 
-  defstruct name: nil, 
-            race: nil, # :pit_bull, :persian, :russian, etc
-            pet_type: nil, # :canine, :feline, :rodent
-            adoption_date: nil,
-            birth_date: nil,
-            age_time: nil,
-            adoption_type: "", # :of_street, :given, :bought, :born
-            age_status: nil, # if actually is a :puppy or adult
-            adoption_age: nil,
-            adoption_status: false,
-            race_size: nil, # :small, :medium, :large, :giant
-            gender: nil,
-            alive_status: :alive,
-            death_date: nil,
-            vaccine_table: %{}
-
-
-  @adult_for_big_dogs 104
-  @adult_for_small_dogs 78
-  @weeks_to_put_in_adoption 12
-            
-
-  @doc """
-  Creates a pet
-  """
-  @spec create_pet(map) :: Pet
-  def create_pet(pet) do
-    pet
-    |> _cast_date_fields()
-    |> _calculate_age_status()
-    |> _cast_params_to_struct()
+  def create_pet(params) do
+    params
+    |> _cast_string_to_date()
+    |> calculate_age_on_weeks()
+    |> calculate_age_status()
+    |> _cast_to_module_struct()
   end
 
-  # Converts a string to Date
-  @spec _cast_date_fields(map) :: map
-  defp _cast_date_fields(
-    %{
-      adoption_date: adoption_date
-    } = pet) when is_binary (adoption_date) do
-    pet
-    |> Map.update!(:adoption_date, &Date.from_iso8601!(&1))
-    |> _cast_date_fields()
-  end
-
-  defp _cast_date_fields(
+  defp _cast_string_to_date(
     %{
       birth_date: birth_date
-    } = pet) when is_binary (birth_date) do
-    pet
+    } = params) when is_binary(birth_date) do
+    params
     |> Map.update!(:birth_date, &Date.from_iso8601!(&1))
-    |> _cast_date_fields()
+    |> _cast_string_to_date()
   end
 
-  # Case when date fields are actually a Date type
-  defp _cast_date_fields(pet), do: pet
+  defp _cast_string_to_date(params), do: params
 
-  # Calculates the age_in_weeks since birth_date when adoption_age is :puppy
-  @spec _calculate_age_weeks_for_pet(map) :: map
-  defp _calculate_age_weeks_for_pet(
+  def calculate_age_on_weeks(%{birth_date: birth_date} = params) do
+    age_on_weeks = _get_weeks_since_birth_date(birth_date)
+    Map.put(params, :age_on_weeks, age_on_weeks)
+  end
+
+  defp _get_weeks_since_birth_date(birth_date) do
+    today = Date.utc_today()
+    Timex.diff(today, birth_date, :weeks)
+  end
+
+  def calculate_age_status(
     %{
-      birth_date: nil
-    }) do
-    "Age must be calculated fot a vet"
-  end
-  
-  defp _calculate_age_weeks_for_pet(
-    %{
-      birth_date: _,
-      adoption_age: :puppy
-    } = pet) do
-    Timex.diff(Date.utc_today(), pet.birth_date, :weeks)
-  end
-
-  # Calculates age_status using race_size
-  #   The :adult age is different for 
-  #   large and giant (104 weeks) races from small
-  #   and medium (78 weeks)
-  #   
-  @spec _calculate_age_status(map) :: map
-  defp _calculate_age_status(%{adoption_age: :adult} = pet) do
-    Map.put(pet, :age_status, :adult)
-  end
-
-  defp _calculate_age_status(
-    %{
-      adoption_age: :puppy, 
-      race_size: race_size,
-    } = pet) when race_size == :small or race_size == :medium do
-
-    case _calculate_age_weeks_for_pet(pet) do
-      weeks when weeks >= @adult_for_small_dogs -> 
-        Map.put(pet, :age_status, :adult)
-      _ -> 
-        Map.put(pet, :age_status, :puppy)
+      race_size: size
+    } = params) when size == :small or size == :medium do
+    case params.age_on_weeks do
+      weeks when weeks < @adult_for_small_dogs ->
+        Map.put(params, :age_status, :puppy)
+      _ ->
+        Map.put(params, :age_status, :adult)
     end
   end
 
-  defp _calculate_age_status(
+  def calculate_age_status(
     %{
-      adoption_age: :puppy, 
-      race_size: race_size,
-    } = pet) when race_size == :large or race_size == :giant do
-
-    case _calculate_age_weeks_for_pet(pet) do
-      weeks when weeks >= @adult_for_big_dogs -> 
-        Map.put(pet, :age_status, :adult)
-      _ -> 
-        Map.put(pet, :age_status, :puppy)
+      race_size: size
+    } = params) when size == :large or size == :giant do
+    case params.age_on_weeks do
+      weeks when weeks < @adult_for_big_dogs ->
+        Map.put(params, :age_status, :puppy)
+      _ ->
+        Map.put(params, :age_status, :adult)
     end
   end
 
-  # Builds a struct whith the given params
-  @spec _cast_params_to_struct(map) :: Pet
-  defp _cast_params_to_struct(pet) do
-    struct(__MODULE__, Map.to_list(pet))
+  defp _cast_to_module_struct(params) do
+    struct(__MODULE__, params)
   end
 
-  @doc """
-  Update the alive_status for a pet
-  """
-  @spec update_death_date_for_pet(Pet, String.t()) :: Pet
-  def update_death_date_for_pet(pet, death_date) do
-    death_date = Date.from_iso8601!(death_date)
-
-    age_time = Timex.diff(death_date, pet.adoption_date, :weeks)
-
-    pet
-    |> Map.replace!(:death_date, death_date)
-    |> Map.put(:age_time, age_time)
-    |> Map.replace!(:alive_status, :death)
-
-  end
-
-  @doc """
-  If a person wants to give in adoption a pet, this function will update
-  the adoption status
-  """
-  @spec update_adoption_status_for_pet(Pet, atom) :: Pet | map
-  def update_adoption_status_for_pet(
-    %Pet{} = %{
-      adoption_age: :puppy
-    } = pet, 
-    adoption_status) do
-    
-    with {:ok, _} <- _check_for_set_in_adoption_availability(pet) do
-      Map.replace!(pet, :adoption_status, adoption_status)
+  def update_adoption_status(pet, adoption_status) do
+    with :ok <- _can_be_set_in_adoption?(pet) do
+      Map.replace(pet, :adoption_status, adoption_status)
     else
       {:error, reason} ->
-        IO.inspect(reason, label: "Error")
         {:error, reason}
     end
   end
-  
-  def update_adoption_status_for_pet(
-    %{
-      adoption_age: :adult
-    } = pet, 
-    adoption_status) do
-    
-    Map.replace!(pet, :adoption_status, adoption_status)
-  end
 
-  # Indicates if the pet can be set in adoption and the reason
-  @spec  _check_for_set_in_adoption_availability(map) :: {boolean, String.t()}
-  defp _check_for_set_in_adoption_availability(pet) do
-    case _calculate_age_weeks_for_pet(pet) do
-      weeks when weeks > @weeks_to_put_in_adoption -> 
-        {:ok, "This pet can be put in adoption"}
-      _ -> 
-        {:error, "Puppies can be put in adoption 12 weeks afer born"}
+  defp _can_be_set_in_adoption?(pet) do
+    case _get_weeks_since_birth_date(pet.birth_date) do
+      weeks when weeks >= @weeks_for_adoption -> :ok
+      _ -> {:error, "Puppies can be set in adoption 12 weeks after born"}
     end
   end
-
 end
--- test/anubis_test.exs
+++ test/anubis_test.exs
@@ -3,195 +3,135 @@ defmodule Anubis.PetTest do
 
   alias Anubis.Pet, as: Pet
 
-  @basic_pet %{
-    name: "Dick",
-    adoption_age: :puppy,
-    adoption_date: "2005-08-24",
-    birth_date: "2005-08-15",
-    pet_type: :canine,
-    race_size: :small,
-    race: "French Mini",
+  @basic_big_puppy_pet %{
+    name: "Rambo",
+    species: :canine,
+    race: "Golden Chocolate",
     gender: :male,
-    adoption_type: :given
-  }
-
-  @adult_pet %{
-    name: "Duquesa",
-    adoption_age: :adult,
-    adoption_date: "2010-04-25",
-    pet_type: :canine,
+    color: :brown,
     race_size: :large,
-    race: "Labrador",
-    gender: :female,
-    adoption_type: :street
+    birth_date: Timex.shift(Date.utc_today(), weeks: -1)
   }
-  
-  test "Creates a basic pet" do
 
-    basic_pet = Pet.create_pet @basic_pet
+  @basic_big_adult_pet %{
+    name: "Rambo",
+    species: :canine,
+    race: "Golden Chocolate",
+    gender: :male,
+    color: :brown,
+    race_size: :large,
+    birth_date: Timex.shift(Date.utc_today(), weeks: -104)
+  }
 
-    assert basic_pet.age_status == :adult
-    assert basic_pet.alive_status == :alive
-  end
+  @basic_small_puppy_pet %{
+    name: "Rambo",
+    species: :canine,
+    race: "Golden Chocolate",
+    gender: :male,
+    color: :brown,
+    race_size: :small,
+    birth_date: Timex.shift(Date.utc_today(), weeks: -1)
+  }
 
-  test "Marks a pet as death" do
-    basic_pet = %{
-      name: "Dick",
-      adoption_age: :puppy,
-      adoption_date: "2005-08-24",
-      pet_type: :canine,
-      race_size: :small,
-      race: "French Mini",
-      gender: :male,
-      adoption_type: :given,
-      birth_date: ~D[2005-07-24]
-    }
+  @basic_small_adult_pet %{
+    name: "Rambo",
+    species: :canine,
+    race: "Golden Chocolate",
+    gender: :male,
+    color: :brown,
+    race_size: :small,
+    birth_date: Timex.shift(Date.utc_today(), weeks: -78)
+  }
 
-    basic_pet = Pet.create_pet basic_pet
+  test "create_pet/1 Can create a pet with basic data" do
+    created_pet = Pet.create_pet @basic_big_puppy_pet
 
-    basic_pet = Pet.update_death_date_for_pet(basic_pet, "2018-09-03")
 
-    assert basic_pet.alive_status == :death
-  end
-
-  test "Accepts Date type on params" do
-    basic_pet = %{
-      name: "Dick",
-      adoption_age: :puppy,
-      adoption_date: ~D[2005-08-24],
-      pet_type: :canine,
-      race_size: :small,
-      race: "French Mini",
-      gender: :male,
-      adoption_type: :given,
-      birth_date: ~D[2005-07-24]
-    }
-
-    pet = Pet.create_pet basic_pet
-
-    assert pet.adoption_date == ~D[2005-08-24]
-  end
-  
-  test "Calculates age status for a large pet" do
-    basic_pet = %{
-      name: "Dick",
-      adoption_age: :puppy,
-      adoption_date: ~D[2005-08-24],
-      birth_date: "2005-06-16",
-      pet_type: :canine,
-      race_size: :large,
-      race: "French Mini",
-      gender: :male,
-      adoption_type: :given
-    }
-
-    pet = Pet.create_pet basic_pet
-
-    assert pet.age_status == :adult
-  end
-
-  test "Calculates age status for an adopted pet when pet is actually an :adult" do
-    pet = Pet.create_pet @adult_pet
-
-    assert pet.age_status == :adult
-  end
-
-  test "Calculates age status for an adopted small pet with few borned weeks" do
-    small_pet = %{
-      name: "Tony",
-      adoption_age: :puppy,
-      adoption_date: ~D[2019-07-14],
-      birth_date: "2019-06-14",
-      pet_type: :canine,
-      race_size: :small,
-      race: "Chihuahua",
-      gender: :male,
-      adoption_type: :given
-    }
-
-    created_pet = Pet.create_pet small_pet
-
+    assert %Anubis.Pet{} = created_pet
+    assert created_pet.species == :canine
+    assert created_pet.name == "Rambo"
+    assert created_pet.gender == :male
+    assert created_pet.race == "Golden Chocolate"
+    assert created_pet.age_on_weeks == 1
     assert created_pet.age_status == :puppy
+
   end
 
-  test "Calculates age status for an adopted large pet with few borned weeks" do
-    small_pet = %{
-      name: "Hulk",
-      adoption_age: :puppy,
-      adoption_date: ~D[2019-07-14],
-      birth_date: "2019-06-14",
-      pet_type: :canine,
-      race_size: :large,
-      race: "PitBull",
-      gender: :male,
-      adoption_type: :given
-    }
+  test "calculate_age_status/1 determinates a small puppy pet" do
+    created_pet = Pet.create_pet @basic_small_adult_pet
 
-    created_pet = Pet.create_pet small_pet
+    assert %Anubis.Pet{} = created_pet
+    assert created_pet.species == :canine
+    assert created_pet.name == "Rambo"
+    assert created_pet.gender == :male
+    assert created_pet.race == "Golden Chocolate"
+    assert created_pet.age_on_weeks == 78
+    assert created_pet.age_status == :adult
 
+  end
+
+  test "calculate_age_status/1 Can determiates a big adlut pet" do
+    created_pet = Pet.create_pet @basic_big_adult_pet
+
+    assert %Anubis.Pet{} = created_pet
+    assert created_pet.species == :canine
+    assert created_pet.name == "Rambo"
+    assert created_pet.gender == :male
+    assert created_pet.race == "Golden Chocolate"
+    assert created_pet.age_on_weeks == 104
+    assert created_pet.age_status == :adult
+
+  end
+
+  test "calculate_age_status/1 Can determiates a small puppy pet" do
+    created_pet = Pet.create_pet @basic_small_puppy_pet
+
+    assert %Anubis.Pet{} = created_pet
+    assert created_pet.species == :canine
+    assert created_pet.name == "Rambo"
+    assert created_pet.gender == :male
+    assert created_pet.race == "Golden Chocolate"
+    assert created_pet.age_on_weeks == 1
     assert created_pet.age_status == :puppy
-  end
- 
-  test "Can set a pet in adoption status" do
-    pet = %{
-      name: "Hulk",
-      adoption_age: :puppy,
-      adoption_date: ~D[2019-06-14],
-      birth_date: "2019-03-14",
-      pet_type: :canine,
-      race_size: :large,
-      race: "PitBull",
-      gender: :male,
-      adoption_type: :given
-    }
 
-    pet = 
-    pet
-    |> Pet.create_pet()
-    |> Pet.update_adoption_status_for_pet(:in_adoption)
-
-    assert pet.adoption_status == :in_adoption
   end
   
-  test "Can not set a pet in adoption with less than 12 borned weeks" do
-    pet = %{
-      name: "Hulk",
-      adoption_age: :puppy,
-      adoption_date: ~D[2019-06-14],
-      birth_date: "2019-11-14",
-      pet_type: :canine,
-      race_size: :large,
-      race: "PitBull",
-      gender: :male,
-      adoption_type: :given
+  test "update_adoption_status/1 Can set in adoption a pet" do
+    pet_map = %{
+      name: "Mila",
+      species: :canine,
+      race: "Schnauser",
+      gender: :female,
+      color: :grey,
+      race_size: :small,
+      birth_date: Timex.shift(Date.utc_today(), weeks: -12)
     }
 
-    pet = 
-    pet
-    |> Pet.create_pet()
-    |> Pet.update_adoption_status_for_pet(:in_adoption)
+    created_pet = 
+    pet_map
+    |> Anubis.Pet.create_pet()
+    |> Anubis.Pet.update_adoption_status(:in_adoption)
 
-    assert {:error, "Puppies can be put in adoption 12 weeks afer born"} = pet
+    assert created_pet.adoption_status == :in_adoption
   end
-  
-  test "Can set a pet in adoption an adult pet" do
-    pet = %{
-      name: "Hulk",
-      adoption_age: :adult,
-      adoption_date: ~D[2015-05-15],
-      birth_date: "2015-04-14",
-      pet_type: :canine,
-      race_size: :large,
-      race: "PitBull",
-      gender: :male,
-      adoption_type: :given
+
+  test "update_adoption_status/1 Can't set in adoption a pet" do
+    pet_map = %{
+      name: "Mila",
+      species: :canine,
+      race: "Schnauser",
+      gender: :female,
+      color: :grey,
+      race_size: :small,
+      birth_date: Timex.shift(Date.utc_today(), weeks: -10)
     }
 
-    pet = 
-    pet
-    |> Pet.create_pet()
-    |> Pet.update_adoption_status_for_pet(:in_adoption)
+    created_pet = 
+    pet_map
+    |> Anubis.Pet.create_pet()
+    |> Anubis.Pet.update_adoption_status(:in_adoption)
 
-    assert pet.adoption_status == :in_adoption
+    assert {:error, reason} = created_pet
+    assert reason == "Puppies can be set in adoption 12 weeks after born"
   end
 end
